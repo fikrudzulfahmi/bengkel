e86eca85ea01f9c39f3492a79ea0639a9772aba6
fikrudzulfahmi <fikrudzulfahmi@gmail.com>
2026-07-27 20:27:07 +0700

    rev seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,19 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
+        User::create([
             'name' => 'Administrator',
             'email' => 'admin@smekisa.com',
-            'password' => bcrypt('password'),
+            'password' => Hash::make('password'),
             'role' => 'admin',
         ]);
         
-        User::factory()->create([
+        User::create([
             'name' => 'Mekanik Handal',
             'email' => 'mekanik@smekisa.com',
-            'password' => bcrypt('password'),
+            'password' => Hash::make('password'),
             'role' => 'mekanik',
         ]);
     }
